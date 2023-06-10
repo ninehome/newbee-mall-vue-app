fcf95650767683d2954c25e2e6c1859632cefd86
neo <stan183183@gmail.com>
2023-06-10 11:33:13 +0800

    个人中心增加刷新

--- src/views/BankView.vue
+++ src/views/BankView.vue
@@ -33,13 +33,18 @@ export default {
       if (item.default==1){
         show =true
       }
+
+      let back =  item.bankNumber.substr(0,4)
+      let front =  item.bankNumber.substr(-4)
+      let bank = back +" **** **** **** "+front
       return {
         id: item.bankId,
         name: item.userName,
         tel: item.bankName,
-        address: item.bankNumber,
+        address: bank,
         isDefault:show
       }
+
     })
   },
   methods: {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -191,7 +191,8 @@ export default {
 
         }).then(res => {
 
-          if (res.data.ShowFlag === 0){
+          // {UserId: 0, MsgText: '', ShowFlag: 0}
+          if (res.data.ShowFlag === 0 && res.data.MsgText !== ""){
             this.ShowMsg(res.data.MsgText)
           }
 
